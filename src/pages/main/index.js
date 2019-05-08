002f13e3bd99d75596eb332eecdf98cb24ed3962
Henrique Tavares <ihenrits@gmail.com>
2019-05-07 23:30:26 -0300

    Interface

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuario/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> (Et et aliqua qui labore fugiat magna irure mollit
+              dolor.){' '}
+            </p>
+            <a href="http://github.com/tavareshenrique">Acessar</a>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
