122fa99665e807855e6548752612d737b1427e19
Henrique Tavares <ihenrits@gmail.com>
2019-05-07 22:46:26 -0300

    Add Todo

--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
-const TodoList = (props) => {
-  console.log(props);
-  return (
+const TodoList = ({ todos, addTodo }) => (
+  <>
     <ul>
-      {props.todos.map(todo => (
+      {todos.map(todo => (
         <li key={todo.id}>{todo.text}</li>
       ))}
     </ul>
-  );
-};
+    <button onClick={() => addTodo('Entrar na Comunidade')}>Adicionar</button>
+  </>
+);
 
 TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
@@ -26,5 +27,11 @@ TodoList.propTypes = {
 const mapStateToProps = state => ({
   todos: state.todos,
 });
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } }),
+});
 
-export default connect(mapStateToProps)(TodoList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
