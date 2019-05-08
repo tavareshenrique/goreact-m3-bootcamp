09f3930ddece1be74530859d14576713335f0963
Henrique Tavares <ihenrits@gmail.com>
2019-05-07 23:12:07 -0300

    Reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 
+import { Provider } from 'react-redux';
 import store from './store';
 
 import TodoList from './pages/TodoList';
+
+console.tron.log('Testando');
 
 function App() {
   return (
